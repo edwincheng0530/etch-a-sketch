57217e31a8a5d79607c460fe86cf9d2a1742a440
Edwin Cheng <edwincheng4840@gmail.com>
2023-01-04 16:51:06 -0500

    Add size-box-changing function

--- javascript.js
+++ javascript.js
@@ -1,32 +1,64 @@
+const dimension = 512;
 const container = document.querySelector('.container');
 
-for(let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.classList.toggle('row');
-    container.appendChild(row);
+function makeEtch(num) {
+    const edge = dimension/num;
+    for(let i = 0; i < num; i++) {
+        const row = document.createElement('div');
+        row.classList.toggle('row');
+        container.appendChild(row);
+    }
+    
+    const rows = document.querySelectorAll('.row');
+    rows.forEach(row => {
+        for(let i = 0; i < num; i++) {
+            const column = document.createElement('div');
+            column.classList.toggle('column');
+            row.appendChild(column);
+        }
+    })
+
+    const squares = document.querySelectorAll('.column');
+    squares.forEach(column => {
+        column.addEventListener('mouseover', function() {
+            column.classList.add('black');
+        })
+        column.style.height = edge + 'px';
+        column.style.width = edge + 'px';
+    });
 }
 
-const rows = document.querySelectorAll('.row');
-rows.forEach(row => {
-    for(let i = 0; i < 16; i++) {
-        const column = document.createElement('div');
-        column.classList.toggle('column');
-        row.appendChild(column);
-    }
-})
 
-const squares = document.querySelectorAll('.column');
-squares.forEach(column => {
-    column.addEventListener('mouseover', function() {
-        column.classList.add('black');
-    })
-});
 
-const clear = document.querySelector('#clear');
+const clear = document.querySelector('.clear');
 clear.addEventListener('click', function() {
+    const squares = document.querySelectorAll('.column');
     squares.forEach(column => column.classList.remove('black'));
 });
 
-const small = document.querySelector('.small');
-const medium = document.querySelector('.medium');
-const large = document.querySelector('.large');
+const size = document.querySelectorAll('.size');
+size.forEach(sizes => {
+    sizes.addEventListener('click', function() {
+        changeSize(sizes.classList);
+    })
+});
+
+function changeSize(classList) {
+    let size = classList[classList.length-1];
+    
+    let child = container.lastElementChild;
+    while(child) {
+        container.removeChild(child);
+        child = container.lastElementChild;
+    }
+
+    if(size == 'small') {
+        makeEtch(16);
+    } else if(size == 'medium') {
+        makeEtch(32);
+    } else {
+        makeEtch(64);
+    }
+}
+
+makeEtch(16);
